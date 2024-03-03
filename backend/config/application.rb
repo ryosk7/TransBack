e747a6f05d3ebd4814f482169e14201dcee6f480
ryosk7 <uchiryo7@gmail.com>
2024-03-04 02:41:16 +0900

    setting test framework.

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -23,5 +23,16 @@ module Backend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                        fixtures: true,
+                        view_specs: false,
+                        helper_specs: false,
+                        routing_specs: false,
+                        request_specs: true,
+                        controller_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
   end
 end
