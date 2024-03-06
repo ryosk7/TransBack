461dda10e8736d212c9a904fc6becdf99f025205
ryosk7 <uchiryo7@gmail.com>
2024-03-07 07:36:10 +0900

    setting metamask

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -7,6 +7,27 @@ import {
 } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { CouponService } from '../services/coupon.service';
+import { createWeb3Modal, defaultConfig } from '@web3modal/ethers5';
+import { environment } from 'src/environments/environment.dev';
+
+const projectId = environment.wc_key;
+
+// 2. Set chains
+const mainnet = {
+  chainId: 1,
+  name: 'Ethereum',
+  currency: 'ETH',
+  explorerUrl: 'https://etherscan.io',
+  rpcUrl: 'https://cloudflare-eth.com',
+};
+
+// 3. Create modal
+const metadata = {
+  name: 'T-Rex',
+  description: 'My Website description',
+  url: 'https://mywebsite.com',
+  icons: ['https://avatars.mywebsite.com/'],
+};
 
 @Component({
   selector: 'app-home',
@@ -23,10 +44,19 @@ import { CouponService } from '../services/coupon.service';
 })
 export class HomePage {
   coupons: any;
+  modal = createWeb3Modal({
+    ethersConfig: defaultConfig({ metadata }),
+    chains: [mainnet],
+    projectId,
+  });
 
   constructor(private service: CouponService) {
     this.service.getCoupons().subscribe((data) => {
       this.coupons = data;
     });
+  }
+
+  openConnectModal() {
+    return this.modal.open();
   }
 }
