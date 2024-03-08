55c7c84fdd63cde669f60b0dab6f1ae42023d8e3
ryosk7 <uchiryo7@gmail.com>
2024-03-08 10:49:32 +0900

    siwe wip

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -8,25 +8,50 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
 import { CouponService } from '../services/coupon.service';
 import { createWeb3Modal, defaultConfig } from '@web3modal/ethers5';
 import { environment } from 'src/environments/environment.dev';
+import { SiweMessage } from 'siwe'
+import { createSIWEConfig } from '@web3modal/siwe'
+import type { SIWECreateMessageArgs, SIWESession, SIWEVerifyMessageArgs } from '@web3modal/siwe'
 
 const projectId = environment.wc_key;
 
-// 2. Set chains
-const testnet = {
-  chainId: 11155111,
-  name: 'Sepolia',
-  currency: 'ETH',
-  explorerUrl: 'https://sepolia.etherscan.io/',
-  rpcUrl: 'https://ethereum-sepolia-rpc.publicnode.com',
-};
+const siweConfig = createSIWEConfig({
+  createMessage: ({ nonce, address, chainId }: SIWECreateMessageArgs) => {
+    new SiweMessage({
+      version: '1',
+      domain: window.location.host,
+      uri: window.location.origin,
+      address,
+      chainId,
+      nonce,
+      statement: 'Sign in With Ethereum.'
+    }).prepareMessage(),
+    getNonce: async () => {
+      const nonce = await getNonce()
+      if (!nonce) {
+        throw new Error('Failed to get nonce!')
+      }
 
-// 3. Create modal
-const metadata = {
-  name: 'ryosk7',
-  description: 'My Website description',
-  url: 'https://mywebsite.com',
-  icons: ['https://avatars.mywebsite.com/'],
-};
+      return nonce
+    }
+  }
+})
+
+// // 2. Set chains
+// const testnet = {
+//   chainId: 11155111,
+//   name: 'Sepolia',
+//   currency: 'ETH',
+//   explorerUrl: 'https://sepolia.etherscan.io/',
+//   rpcUrl: 'https://ethereum-sepolia-rpc.publicnode.com',
+// };
+// 
+// // 3. Create modal
+// const metadata = {
+//   name: 'ryosk7',
+//   description: 'My Website description',
+//   url: 'https://mywebsite.com',
+//   icons: ['https://avatars.mywebsite.com/'],
+// };
 
 @Component({
   selector: 'app-home',
