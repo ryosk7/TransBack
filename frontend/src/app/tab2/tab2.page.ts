8908fe71061b92107bd1be37b0ad480f0e97ff75
ryosk7 <uchiryo7@gmail.com>
2024-03-09 16:45:45 +0900

    Add connection button in account page.

--- frontend/src/app/tab2/tab2.page.ts
+++ frontend/src/app/tab2/tab2.page.ts
@@ -2,6 +2,27 @@ import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { CouponService } from '../services/coupon.service';
+import { environment } from 'src/environments/environment.dev';
+import { createWeb3Modal, defaultConfig } from '@web3modal/ethers5';
+
+const projectId = environment.wc_key;
+
+// 2. Set chains
+const testnet = {
+  chainId: 11155111,
+  name: 'Sepolia',
+  currency: 'ETH',
+  explorerUrl: 'https://sepolia.etherscan.io/',
+  rpcUrl: 'https://ethereum-sepolia-rpc.publicnode.com',
+};
+
+// 3. Create modal
+const metadata = {
+  name: 'ryosk7',
+  description: 'My Website description',
+  url: 'https://mywebsite.com',
+  icons: ['https://avatars.mywebsite.com/'],
+};
 
 @Component({
   selector: 'app-tab2',
@@ -12,11 +33,20 @@ import { CouponService } from '../services/coupon.service';
 })
 export class Tab2Page {
   coupons: any;
+  modal = createWeb3Modal({
+    ethersConfig: defaultConfig({ metadata }),
+    chains: [testnet],
+    projectId,
+  });
 
   constructor(private couponService: CouponService) {
     this.couponService.getCoupons().subscribe((data) => {
       this.coupons = data;
     });
+  }
+
+  openConnectModal() {
+    return this.modal.open();
   }
 
 }
