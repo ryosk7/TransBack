0147850fde3a3ae03773193646582ec6c6370d6c
ryosk7 <uchiryo7@gmail.com>
2024-03-13 19:25:08 +0900

    fix: env with coupon service.

--- frontend/src/app/services/coupon.service.ts
+++ frontend/src/app/services/coupon.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 export type Coupon = {
   id: number;
@@ -14,23 +15,23 @@ export type Coupon = {
   providedIn: 'root',
 })
 export class CouponService {
-  url = 'http://localhost:3000';
+  private _url = environment.url;
 
   constructor(private httpClient: HttpClient) {}
 
   getCoupons() {
-    return this.httpClient.get<Coupon[]>(`${this.url}/coupons`);
+    return this.httpClient.get<Coupon[]>(`${this._url}/coupons`);
   }
 
   myCoupons(user_id: number) {
     let params = new HttpParams().set('user_id', user_id);
-    return this.httpClient.get<Coupon[]>(`${this.url}/user_coupons`, {
+    return this.httpClient.get<Coupon[]>(`${this._url}/user_coupons`, {
       params,
     });
   }
 
   createUserCoupon(couponId: number, userId: number) {
-    return this.httpClient.post(`${this.url}/user_coupons`, {
+    return this.httpClient.post(`${this._url}/user_coupons`, {
       coupon_id: couponId,
       user_id: userId,
     });
